fcc0fdb4df53bafc2108e3f3aa9e92ea216a152f
nick mason <nmason197@gmail.com>
2022-07-31 22:10:15 -0400

    v 1.1

--- App.js
+++ App.js
@@ -1,13 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet,Button,TextInput, ActivityIndicator } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import P2_Questionaire from './components/P2_Questionaire'
+import { NativeBaseProvider } from "native-base";
+import Home from './components/Home'
+import P3_Questionaire from './components/P3_Questionaire';
+import P1_Questionaire from './components/P1_Questionaire';
+import { deleteUsers,createTable, dropTable, turnForeignKeysOn } from './storage/Database';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [appState,setAppState] = useState("questioning")
+  const [loading,setLoading] = useState(true)
+  turnForeignKeysOn()
+  dropTable()
+  createTable("users")
+  createTable("interests")
+  createTable("availability")
+  createTable("schedule")
+
+
+
+  const getLoginState = async () => {
+    try {
+      await AsyncStorage.getItem('appState').then((appState)=>{  appState == "passed"? setAppState("passed") : setAppState("questioning")   })
+    } catch (e) {
+      // saving error
+    }
+  }
+
+
+  useEffect(() => {
+
+    getLoginState()
+    console.log(appState)
+    setLoading(false)
+  },[])
+  // options={{headerBackVisible:false}}
+  if(loading) {return <View style={styles.container}><ActivityIndicator/></View>}
+   else if(appState == "passed" && !loading)
+return (
+    <NativeBaseProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        
+        <Stack.Screen name="p1_questionaire" component={P1_Questionaire} />
+        <Stack.Screen name="p2_questionaire" component={P2_Questionaire} />
+        <Stack.Screen name="p3_questionaire" component={P3_Questionaire} />
+       <Stack.Screen name ="Home" component={Home}/> 
+      </Stack.Navigator>
+    </NavigationContainer>
+    </NativeBaseProvider>
   );
+  else return     <NativeBaseProvider>
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name ="Home" component={Home}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+  </NativeBaseProvider>
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +70,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    width:'100%'
   },
+  btn:{
+
+  },
+  input:{
+width:'80%',
+height:50,
+borderWidth:2
+  },
+  headerContainer:{
+    flex:.5,
+    justifyContent:'center'
+  },
+  inputContainer:{
+    flex:.5,
+    width:'100%',
+    justifyContent:'flex-start',
+    alignItems:'center'
+  },
+  btnContainer:{
+    flex:1,
+    width:'100%'
+  }
 });
